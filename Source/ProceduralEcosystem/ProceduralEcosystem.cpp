73925c2c13998da1e1637e70faf6969b4e892ad9
Izana <izanaluque@gmail.com>
2026-07-06 17:17:02 +0200

    Proyecto UE5 en blanco con módulo Landscape, compilando

---
+++ Source/ProceduralEcosystem/ProceduralEcosystem.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "ProceduralEcosystem.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, ProceduralEcosystem, "ProceduralEcosystem" );
